745ffe77047cff006cdf7a3e5bc0caebcafecabf
Илья Орлов ИТ-13 <ilyaorlovv6534@gmail.com>
2025-05-21 01:37:54 +0500

    Update config.py

--- config.py
+++ config.py
@@ -23,28 +23,38 @@ ODM_DOCKER_IMAGE = 'opendronemap/odm:latest' # Docker образ ODM (можно
 ODM_PROJECT_NAME = "odm_processing"
 
 ODM_OPTIONS = {  
-    
+
     "use-gpu": True,                  # Пытаться использовать GPU?
     "max-concurrency": max(1, multiprocessing.cpu_count() // 2),
+                                      # Максимальное количество потоков
+    
     # --- Входные данные ---
-    # "resize-to": 2400,              # Уменьшить изображение до 2400 px по большей стороне перед обработкой
+    "resize-to": 1000,                # Уменьшить изображение до 2400 px по большей стороне перед обработкой
     "use-exif": True,                 # Использовать GPS-данные из EXIF изображений
 
     # --- Этап извлечения признаков (Feature Extraction) и сопоставления (Matching) ---
-    "feature-quality": "ultra",       # Качество извлечения признаков (ultra|high|medium|low|lowest)
+    "feature-quality": "lowest",      # Качество извлечения признаков (ultra|high|medium|low|lowest)
     "matcher-type": "flann",          # Тип алгоритма сопоставления признаков (flann|bow|sequential|bruteforce|spatial)
     # "flann-neighbors": <int>
     # "bow-num-words": <int> (если "matcher-type": "bow") количество слов в словаре
 
     # --- Этап плотного восстановления (MVS) ---    
-    "pc-quality": "ultra",            # Качество плотного облака точек
+    "pc-quality": "lowest",           # Качество плотного облака точек
+    
+    # --- Этап Создания 3D Меша (Meshing) ---
+    "skip-3dmesh": True,
+
+    # --- Этап Текстурирования Меша (Texturing) ---
+    "skip-texturing": True,
 
     # --- Этап создания цифровых моделей (DEM, DSM, DTM) ---
     "dsm": True,                      # Генерировать цифровую модель поверхности
 
     # --- Этап создания ортофотоплана ---
-    "orthophoto-resolution": 2.0,     # Разрешение ортофото в СМ/пиксель (каждый пиксель на ортофотоплане будет представлять квадрат со стороной 2 см на реальной местности)
-    "fast-orthophoto": False,         # Использовать быстрый метод генерации
+    "orthophoto-resolution": 20.0,    # Разрешение ортофото в СМ/пиксель (каждый пиксель на ортофотоплане будет представлять квадрат со стороной 2 см на реальной местности)
+    "fast-orthophoto": True,          # Использовать быстрый метод генерации
+
+    
 }
 
 # --- Параметры анализа парковок (для analysis.py) ---
